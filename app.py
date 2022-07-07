2d2ed4c3df733906dd42da2fbc0aef314bff7add
khurram shakoor <73300634+khurram-DS@users.noreply.github.com>
2022-07-07 19:12:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,14 +110,16 @@ in their work.
         if st.checkbox("Dates in the Format of YYYY-MM-DD"):
             df[selected_columns]=pd.to_datetime(df[selected_columns],errors='coerce')
             df[selected_columns]=df[selected_columns].dt.strftime("%Y-%m-%d")
+            df=df
             st.write(df.head())
             st.text("Download the Above Data table by clicking on Download CSV")
-            df=df
+           
             st.download_button(label='Download CSV',data=df.to_csv(),mime='text/csv')
         st.write("**3. Download the clean Data in Format of YYYY:MM:DD HH:MM:SS**")
         if st.checkbox("Dates in the Format of YYYY-MM-DD HH:MM:SS"):
             df[selected_columns]=pd.to_datetime(df[selected_columns],errors='coerce')
             df[selected_columns]=df[selected_columns].dt.strftime("%Y-%m-%d %H:%M:%S")
+            df=df
             st.write(df.head())
             st.text("Download the Above Data table by clicking on Download CSV")
             df=df
