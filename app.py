eb69715014e4b4c246911eb975ad6b328f65b632
khurram shakoor <73300634+khurram-DS@users.noreply.github.com>
2022-07-13 12:15:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ in their work.
           ds=[]
           ds=df
           st.text("Download the Above Data table by clicking on Download CSV")
-          st.download_button(label='Download CSV',data=ds.to_csv(),mime='text/csv')
+          #st.download_button(label='Download CSV',data=ds.to_csv(),mime='text/csv')
                 
            
                 
